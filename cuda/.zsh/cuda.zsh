962b11fabe0226cdf4e3751da1344fa73df96550
Martijn Courteaux <courteauxmartijn@gmail.com>
2018-10-26 10:03:50 +0200

    Fixed cuda + extra etc/default/keyboard file.

--- cuda/.zsh/cuda.zsh
+++ cuda/.zsh/cuda.zsh
@@ -1,2 +1,3 @@
 export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export CUPIT_LIB_PATH=/usr/local/cuda-9.0/extras/CUPTI/lib64
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64:${CUPIT_LIB_PATH}:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
