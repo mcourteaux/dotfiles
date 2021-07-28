de1638d06d37de48a25592873177c1c11289c1ba
Martijn Courteaux <courteauxmartijn@gmail.com>
2021-07-29 00:30:59 +0200

    High cpu load warning works as launchagent.

--- watch-cpu/.bin/watchcpu.sh
+++ watch-cpu/.bin/watchcpu.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-read pct name < <(top -l 4 -n 1 -F -o cpu -stats cpu,command | tail -1)
-if (( ${pct%.*} >= 30 )); then
-  terminal-notifier -sound purr -title "High CPU usage" -message "$name ($pct%)" -execute "open -a 'Activity Monitor'"
-fi
+while true; do
+    read pct name < <(top -l 8 -n 1 -F -o cpu -stats cpu,command | tail -1)
+    if (( ${pct%.*} >= 40 )); then
+        /usr/local/bin/terminal-notifier -sound purr -title "High CPU usage" -message "$name ($pct%)" -execute "open -a 'Activity Monitor'"
+    fi
+    sleep 5
+done
 
