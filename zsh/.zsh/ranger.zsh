c7ae6ea0fa1e6febb83f70d3f238f909ab68d9c2
Martijn Courteaux <courteauxmartijn@gmail.com>
2022-02-09 16:15:03 +0100

    Ranger CD

---
+++ zsh/.zsh/ranger.zsh
@@ -0,0 +1,16 @@
+
+rangercd () {
+    tmp="$(mktemp)"
+    ranger --choosedir="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        if [ -d "$dir" ]; then
+            if [ "$dir" != "$(pwd)" ]; then
+                cd "$dir"
+            fi
+        fi
+    fi
+}
+
+bindkey -s '^o' 'rangercd\n'
