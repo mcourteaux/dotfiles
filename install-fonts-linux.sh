073d520304fd1da02924a224175858e0f18a2aae
Martijn Courteaux <courteauxmartijn@gmail.com>
2024-06-09 21:28:25 +0200

    Fix install fonts.

--- install-fonts-linux.sh
+++ install-fonts-linux.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-VERSION="v3.1.1"
-mkdir tmp-fonts
+VERSION="v3.2.1"
+
+rm -rf tmp-fonts
+
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/$VERSION/RobotoMono.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/$VERSION/FiraCode.zip
 
-mkdir tmp-fonts && cd tmp-fonts
+mkdir -p tmp-fonts && cd tmp-fonts
 
 # Fira Code
 unzip ../FiraCode.zip
-sudo mkdir /usr/share/fonts/firacode-nerdfont/
+sudo mkdir -p /usr/share/fonts/firacode-nerdfont/
 sudo mv * /usr/share/fonts/firacode-nerdfont/
 
 unzip ../RobotoMono.zip
-sudo mkdir /usr/share/fonts/robotomono-nerdfont/
+sudo mkdir -p /usr/share/fonts/robotomono-nerdfont/
 sudo mv * /usr/share/fonts/firacode-nerdfont/
 
 cd ..
