991c3f202d80c9f2d0f469e96aa6d911b036ec59
Martijn Courteaux <courteauxmartijn@gmail.com>
2024-09-29 10:40:05 +0200

    Hyprlock

--- hypr-desktop/.local/bin/right_rot.sh
+++ hypr-desktop/.local/bin/right_rot.sh
@@ -3,4 +3,14 @@ echo "
 monitor = DP-3, 3440x1440, 0x0      , 1
 monitor = DP-1, 2560x1440, 3440x-200, 1, transform, 3
 monitor = DP-2, disabled
+
+workspace = 1, monitor:DP-1
+workspace = 2, monitor:DP-1
+workspace = 3, monitor:DP-1
+workspace = 4, monitor:DP-3
+workspace = 5, monitor:DP-3
+workspace = 6, monitor:DP-3
+workspace = 7, monitor:DP-3
+workspace = 8, monitor:DP-3
+workspace = 9, monitor:DP-3
 " > $HOME/.config/hypr/monitors.conf
