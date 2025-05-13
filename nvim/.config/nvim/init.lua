d2c8233282bb4e158f0186515ff137925fc28534
Martijn Courteaux <courteauxmartijn@gmail.com>
2025-05-13 14:03:56 +0200

    Outline plugin.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -74,6 +74,7 @@ require("lazy").setup({
     --  aca.setup({})
     --end
   },
+
   -- LSP
   { 'neovim/nvim-lspconfig' },
   {
@@ -147,6 +148,18 @@ require("lazy").setup({
          }
        })
      end
+  },
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+      { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
   },
 
   -- Colorschemes
